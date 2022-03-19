c758d63c7d8c06e27a75a4742701605d78bcbb9c
Piscki <pisckipratama@gmail.com>
2022-03-19 07:04:48 +0700

    add nilai mahasiswa and template

---
+++ Praktikum_3/template-web/index.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'header.php';
+?>
+
+<?php
+include_once 'navbar.php';
+?>
+
+<h1 class="mt-4">Welcome to Dashboard, Guest User!</h1>
+
+<?php
+include_once 'content.php';
+?>
+
+<?php
+include_once 'footer.php';
