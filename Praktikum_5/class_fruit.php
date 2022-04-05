3f7a2d6705a6d98404b98da74aa57442518fb827
Piscki <pisckipratama@gmail.com>
2022-04-06 06:09:18 +0700

    add class_fruit

---
+++ Praktikum_5/class_fruit.php
@@ -0,0 +1,32 @@
+<?php
+
+class Fruit
+{
+  public $name;
+  public $color;
+  private $weight;
+
+  function set_name($n)
+  {
+    $this->name = $n;
+  }
+
+  protected function set_color($n)
+  {
+    $this->color = $n;
+  }
+
+  private function set_weight($n)
+  {
+    $this->weight;
+  }
+}
+
+$mango = new Fruit();
+$mango->name = 'Mango'; // OK
+$mango->color = 'Yellow'; // ERROR
+$mango->weight = 300; // Error
+
+$mango->set_name('Mangga'); // OK
+$mango->set_color('Green'); // ERROR
+$mango->set_weight('200'); // ERROR
