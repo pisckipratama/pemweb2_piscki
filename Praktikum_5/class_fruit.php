7df32cce4c2843b5fbf492cad83760a7f6133083
Piscki <pisckipratama@gmail.com>
2022-04-07 05:54:50 +0700

    add inheritence in class fruit

--- Praktikum_5/class_fruit.php
+++ Praktikum_5/class_fruit.php
@@ -6,6 +6,12 @@ class Fruit
   public $color;
   private $weight;
 
+  public function __construct($name, $color)
+  {
+    $this->name = $name;
+    $this->color = $color;
+  }
+
   function set_name($n)
   {
     $this->name = $n;
@@ -20,15 +26,33 @@ class Fruit
   {
     $this->weight;
   }
+
+  public function intro()
+  {
+    echo "Nama buah ini adalah {$this->name} dan warnanya {$this->color}";
+  }
 }
 
-$mango = new Fruit();
-$mango->name = 'Mango'; // OK
-echo $mango->name . '<br/>';
+// $mango = new Fruit();
+// $mango->name = 'Mango'; // OK
+// echo $mango->name . '<br/>';
 // $mango->color = 'Yellow'; // ERROR
 // $mango->weight = 300; // Error
 
-$mango->set_name('Mangga'); // OK
+// $mango->set_name('Mangga'); // OK
 // $mango->set_color('Green'); // ERROR
 // $mango->set_weight('200'); // ERROR
-echo $mango->name;
+// echo $mango->name;
+
+class Strawberry extends Fruit
+{
+  public function message()
+  {
+    echo "apakah saya adalah buah atau berry?";
+  }
+}
+
+
+$strawberry = new Strawberry("Strawberry", "Merah");
+$strawberry->message();
+$strawberry->intro();
