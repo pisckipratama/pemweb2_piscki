1756b86eff2c4957edf5e562ac20e9d5fbe85792
Piscki <pisckipratama@gmail.com>
2022-04-23 14:50:36 +0700

    add readme and delete modal

---
+++ Proyek1_UTS/README.md
@@ -0,0 +1,7 @@
+# Tugas Proyek Praktikum Web 2
+
+Pada tugas ini diminta untuk membuat website dinamis menghitung BMI menggunakan AdminLTE dan PHP.
+
+hasil dari kerja ini bisa diakses pada link berikut [https://kreasi.nurulfikri.ac.id/pisc21001ti/web2/proyek1](https://kreasi.nurulfikri.ac.id/pisc21001ti/web2/proyek1)
+
+Ini semua dibuat oleh [Piscki](https://www.instagram.com/pisckiprtm)
--- Proyek1_UTS/kalkulator.php
+++ Proyek1_UTS/kalkulator.php
@@ -121,31 +121,11 @@ $ar_pasien = [$pasienbmi1, $pasienbmi2, $pasienbmi3];
               <input id="tinggi" name="tinggi" placeholder="Tinggi Badan" type="text" class="form-control" required="required">
             </div>
             <div class="form-group">
-              <button name="submit" type="submit" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Simpan</button>
+              <button name="submit" type="submit" class="btn btn-primary">Simpan</button>
             </div>
           </form>
         </div>
         <!-- /.card-body -->
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
       <div class="card">
         <div class="card-header">
