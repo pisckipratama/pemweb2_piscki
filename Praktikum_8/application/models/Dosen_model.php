2ce2d024368f4a446932a41e6ff607a06522bb4c
Piscki <pisckipratama@gmail.com>
2022-06-20 09:20:32 +0700

    init praktikum 8

---
+++ Praktikum_8/application/models/Dosen_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class Dosen_model extends CI_Model
+{
+	public $id;
+	public $nama;
+	public $nidn;
+	public $pendidikan;
+	public $gender;
+	public $tmp_lahir;
+	public $tgl_lahir;
+}
---
+++ Praktikum_8/application/models/Mahasiswa_model.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mahasiswa_model extends CI_Model
+{
+	public $id;
+	public $nama;
+	public $nim;
+	public $gender;
+	public $tmp_lahir;
+	public $tgl_lahir;
+	public $ipk;
+
+	public function predikat()
+	{
+		$predikat = ($this->ipk >= 3.75) ? "Cumlaude" : "Baik";
+		return $predikat;
+	}
+}
